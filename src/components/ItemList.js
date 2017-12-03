e724d593a0ec898e60e2ed667580200d4e37061e
rias-z <chestermx88@gmail.com>
2017-12-03 22:19:48 +0900

    [WEB-4] Itemのdeveloperを表示する

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -17,7 +17,16 @@ class ItemList extends Component{
       <div>
         <h2>ItemList</h2>
         {this.props.items.itemList.map(item => (
-          <div key={item.id}>{item.id}: {item.name}, {item.description}</div>
+          <div key={item.id}>
+            {item.id}: {item.name}, {item.description}
+            <div>
+              developer:
+              {item.developers.map((user_name, i) => (
+                <div key={i}>{user_name},</div>
+              ))}
+            </div>
+            <br />
+          </div>
         ))}
       </div>
     )
